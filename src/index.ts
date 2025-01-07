9173854061abe2f08847588a9b89590b34bc9685
diasEduardo <eduardo_dududex@hotmail.com>
2025-01-07 17:45:15 -0300

    change socket events files

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import { createFolderOnInit } from './util/utility';
 import { getGroupChat } from './Store/ChatStore';
 
 
-const boundary = createBoundary({
+export const boundary = createBoundary({
 	url: `${process.env.GATEWAY_URL}`,
 	chatPlatform: 'Baileys',
 	name: `${process.env.BOUNDARY_ID}`,
@@ -20,41 +20,6 @@ createFolderOnInit();
 
 initSession('tramont').then((waSocket:any) => {
 	const baileys = baileysFunctions(waSocket);
-
-	waSocket.ev.on('messages.upsert', async (upsert:any) => {
-		
-		for (const msg of upsert.messages) {
-			//console.log(msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.viewOnceMessage)
-			//console.log(msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.viewOnceMessageV2?.message)
-			console.log(`processando mensagem ${msg.key.id}`)
-			if(msg.message?.stickerMessage){
-				msg.message.stickerMessage.url = `https://mmg.whatsapp.net${msg.message.stickerMessage.directPath}`
-			}
-			
-			try {
-				const payload = await createMessagePayload(msg, waSocket);
-				if (Context.get('blockedList').includes(payload.from)) {
-					return;
-				}
-
-				await saveMessage(payload, msg);
-				// console.log(
-				// 	JSON.stringify(
-				// 		{
-				// 			body: payload.body,
-				// 			author: payload.contact.id,
-				// 			msg,
-				// 		},
-				// 		undefined,
-				// 		'  '
-				// 	)
-				// );
-				boundary.emitMessage(payload);
-			} catch (e) {
-				console.warn(e);
-			}
-		}
-	});
 
 	boundary.handleReplyWithText((payload, companion, body) => {
 		baileys.sendText(payload.chatId, body, payload.quoteId, companion.mentions);
